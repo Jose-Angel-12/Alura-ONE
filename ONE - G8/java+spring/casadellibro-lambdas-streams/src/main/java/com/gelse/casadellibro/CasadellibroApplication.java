20417a2b26e6f4e7d33c1bf9bca7f278a6a86fc6
Jose-Angel-12 <jajaraba12@gmail.com>
2025-06-09 17:13:10 -0500

    update

--- ONE - G8/java+spring/casadellibro-lambdas-streams/src/main/java/com/gelse/casadellibro/CasadellibroApplication.java
+++ ONE - G8/java+spring/casadellibro-lambdas-streams/src/main/java/com/gelse/casadellibro/CasadellibroApplication.java
@@ -11,7 +11,7 @@ public class CasadellibroApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(CasadellibroApplication.class, args);
 	}
-
+	//comentario
 	@Override
 	public void run(String... args) throws Exception {
 		Principal principal = new Principal();
