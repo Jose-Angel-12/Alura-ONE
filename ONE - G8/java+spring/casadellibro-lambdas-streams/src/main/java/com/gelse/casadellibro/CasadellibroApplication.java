883e22cf58f7a4811515944014ec858b96eebb80
Jose-Angel-12 <jajaraba12@gmail.com>
2025-06-09 17:26:49 -0500

    saaf

--- ONE - G8/java+spring/casadellibro-lambdas-streams/src/main/java/com/gelse/casadellibro/CasadellibroApplication.java
+++ ONE - G8/java+spring/casadellibro-lambdas-streams/src/main/java/com/gelse/casadellibro/CasadellibroApplication.java
@@ -11,6 +11,9 @@ public class CasadellibroApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(CasadellibroApplication.class, args);
 	}
+	//comentario
+	/// ootro comentario
+	/// /sadgag
 	@Override
 	public void run(String... args) throws Exception {
 		Principal principal = new Principal();
