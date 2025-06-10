5928158005ce19fd8a688c30b343c45305a96efc
Jose-Angel-12 <jajaraba12@gmail.com>
2025-06-10 16:47:02 -0500

    update

--- conversor-moneda/src/lad/com/alura/conversormoneda/ConversorApp.java
+++ conversor-moneda/src/lad/com/alura/conversormoneda/ConversorApp.java
@@ -2,6 +2,7 @@ package lad.com.alura.conversormoneda;
 
 import java.io.IOException;
 
+//ajnbiajbf
 public class ConversorApp {
     public static void main(String[] args) throws IOException, InterruptedException {
         Conversor conversor = new Conversor();
