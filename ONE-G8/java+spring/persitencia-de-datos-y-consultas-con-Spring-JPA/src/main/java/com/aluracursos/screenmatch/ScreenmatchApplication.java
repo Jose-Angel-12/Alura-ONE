0cc24e3b9aca7b743ee873a8f9c31993f863acf3
Jose-Angel-12 <jajaraba12@gmail.com>
2025-06-13 21:48:24 -0500

    probando base de datos

--- ONE-G8/java+spring/persitencia-de-datos-y-consultas-con-Spring-JPA/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ ONE-G8/java+spring/persitencia-de-datos-y-consultas-con-Spring-JPA/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,17 +1,17 @@
-package com.aluracursos.screenmatch;
-
-import com.aluracursos.screenmatch.principal.Principal;
-import com.aluracursos.screenmatch.repository.SerieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class ScreenmatchApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(ScreenmatchApplication.class, args);
-//server.port=8081 se agregar a application.propierties por si esta ocupado el puerto 8080
-    }
-}
+//package com.aluracursos.screenmatch;
+//
+//import com.aluracursos.screenmatch.principal.Principal;
+//import com.aluracursos.screenmatch.repository.SerieRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class ScreenmatchApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(ScreenmatchApplication.class, args);
+////server.port=8081 se agregar a application.propierties por si esta ocupado el puerto 8080
+//    }
+//}
